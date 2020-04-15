8ef47a01b5c28e9816f612008b8b28d19124207c
unknown <hari29harran@gmail.com>
2020-04-15 16:50:12 +0530

    Updates

---
+++ precache-manifest.ea73be12692637424dcbae5a05b20676.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11490e892670dd3e39dd46db2ac0e53e",
+    "url": "/learn/index.html"
+  },
+  {
+    "revision": "63fffcbdd983e2ddbedc",
+    "url": "/learn/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "fe2bcd989bf3c75bc2d9",
+    "url": "/learn/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "63fffcbdd983e2ddbedc",
+    "url": "/learn/static/js/2.1cff1f38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learn/static/js/2.1cff1f38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe2bcd989bf3c75bc2d9",
+    "url": "/learn/static/js/main.7e4301ce.chunk.js"
+  },
+  {
+    "revision": "ba1497393eaa5de9ee16",
+    "url": "/learn/static/js/runtime-main.d9e38610.js"
+  }
+]);
